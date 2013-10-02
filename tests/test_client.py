44e13754472b69ef01b67d399e58d71249f7a622
swistakm <swistakm@gmail.com>
2013-10-02 16:28:58 +0200

    add tests for api_key

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+import pytest
+
+from gmaps import errors
+from gmaps import Geocoding
+
+def test_client_with_key():
+    geocoding = Geocoding(api_key="invalid_key", use_https=True)
+    with pytest.raises(errors.RequestDenied) as excinfo:
+        geocoding.geocode("Some address")
+
+    assert "API key is invalid" in str(excinfo)
