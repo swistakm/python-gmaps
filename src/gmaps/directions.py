1374adbdc372b21cc04c8e488ef00e1f2b82134a
swistakm <swistakm@gmail.com>
2013-10-03 14:47:18 +0200

    reformat code

--- src/gmaps/directions.py
+++ src/gmaps/directions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from gmaps.client import Client
 
+
 class Directions(Client):
     DIRECTIONS_URL = 'directions/'
 
@@ -10,7 +11,7 @@ class Directions(Client):
             output = place
         elif isinstance(place, dict):
             try:
-                 output = "%f,%f" % (place["lat"], place["lon"])
+                output = "%f,%f" % (place["lat"], place["lon"])
             except KeyError:
                 raise TypeError("%s is invalid place object" % str(place))
         elif hasattr(place, "__iter__") and len(place) == 2:
@@ -18,15 +19,15 @@ class Directions(Client):
         elif hasattr(place, "lat") and hasattr(place, "lon"):
             output = "%f,%f" % (place.lat, place.lon)
         else:
-                raise TypeError("%s is invalid place object" % str(place))
+            raise TypeError("%s is invalid place object" % str(place))
         return output
 
-
-    def directions(self, origin, destination,
-                   mode=None, alternatives=None, avoid=None, language=None, units=None,
+    def directions(self, origin, destination, mode=None, alternatives=None,
+                   avoid=None, language=None, units=None,
                    region=None, departure_time=None, arrival_time=None):
         parameters = dict(
             origin=self.latlon_or_address(origin),
             destination=self.latlon_or_address(destination),
+            mode=mode,
         )
         return self._make_request(self.DIRECTIONS_URL, parameters, "routes")
