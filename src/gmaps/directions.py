ddb87c682d4e8605bc97a30f29caacc19a97fca0
swistakm <swistakm@gmail.com>
2013-10-03 15:54:59 +0200

    document directions API

--- src/gmaps/directions.py
+++ src/gmaps/directions.py
@@ -25,9 +25,41 @@ class Directions(Client):
     def directions(self, origin, destination, mode=None, alternatives=None,
                    avoid=None, language=None, units=None,
                    region=None, departure_time=None, arrival_time=None):
+        """Get directions between locations
+
+        :param origin: Origin location - string address; (latitude, longitude)
+            two-tuple, dict with ("lat", "lon") keys or object with (lat, lon)
+            attributes
+        :param destination: Destination location - type same as origin
+        :param mode: Travel mode as string, defaults to "driving".
+            See `google docs details <https://developers.google.com/maps/documentation/directions/#TravelModes>`
+        :param alternatives: True if provide it has to return more then one
+            route alternative
+        :param avoid: Iterable with set of restrictions,
+            like ("tolls", "highways"). For full list refer to
+            `google docs details <https://developers.google.com/maps/documentation/directions/#Restrictions>`
+        :param language: The language in which to return results.
+            See `list of supported languages <https://developers.google.com/maps/faq#languagesupport>`
+        :param units: Unit system for result. Defaults to unit system of
+            origin's country.
+            See `google docs details <https://developers.google.com/maps/documentation/directions/#UnitSystems>`
+        :param region: The region code. Affects geocoding of origin and
+            destination (see `gmaps.Geocoding.geocode` region parameter)
+        :param departure_time: Desired time of departure as
+            seconds since midnight, January 1, 1970 UTC
+        :param arrival_time: Desired time of arrival for transit directions as
+            seconds since midnight, January 1, 1970 UTC.
+        """
         parameters = dict(
             origin=self.latlon_or_address(origin),
             destination=self.latlon_or_address(destination),
             mode=mode,
+            alternatives=alternatives,
+            avoid=avoid,
+            language=language,
+            units=units,
+            region=region,
+            departure_time=departure_time,
+            arrival_time=arrival_time,
         )
         return self._make_request(self.DIRECTIONS_URL, parameters, "routes")
