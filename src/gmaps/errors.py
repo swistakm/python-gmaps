fbfd785cffa8a1acc879b0aa92ce2591ea94f0e6
swistakm <swistakm@gmail.com>
2013-10-03 14:37:21 +0200

    PEP-8

--- src/gmaps/errors.py
+++ src/gmaps/errors.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 import status
 
+
 class GmapException(BaseException):
     """Base exception for all python-gmap exceptions"""
+
 
 class NoResults(GmapException):
     """Raised when api returned no results"""
 
+
 class RequestDenied(GmapException):
     """Raised when request to API was denied"""
+
 
 EXCEPTION_MAPPING = {
     status.OK: None,
