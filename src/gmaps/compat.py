6c09980c743c0ab3e0c1c6a28fd3665e5f734a67
mjaworski <mjaworski@opera.com>
2016-03-22 18:52:54 +0100

    CI: add new entries to test matrix and add pep8 exempt

--- src/gmaps/compat.py
+++ src/gmaps/compat.py
@@ -5,7 +5,7 @@ if sys.version_info < (3, 0, 0):
     import urlparse  # noqa
 
     def is_string(s):
-        return isinstance(s, basestring)
+        return isinstance(s, basestring)  # noqa
 
 else:
     from urllib import parse as urlparse  # noqa
